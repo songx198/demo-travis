7becaee367ab39271b96865b30da10e32e14fcf1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-19 04:07:15 +0000

    Deploy songx198/demo-travis to github.com/songx198/demo-travis.git:gh-pages

---
+++ precache-manifest.b4b3f17f681be217d432aa76537cf572.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f08ca80121a18f1db898fc00a6406a08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e67a130449d74ea388bb",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "dffd882bfcb2205d6cf4",
+    "url": "/static/js/2.dde2ca4e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.dde2ca4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e67a130449d74ea388bb",
+    "url": "/static/js/main.cd732fbb.chunk.js"
+  },
+  {
+    "revision": "8e2447860db7787073a8",
+    "url": "/static/js/runtime-main.653fe643.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
