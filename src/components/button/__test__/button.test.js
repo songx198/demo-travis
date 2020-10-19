62b1dd15f5856cdaaeedeb76a2c4a5483e77dce7
songx198 <moko1125986@gmail.com>
2020-10-18 23:40:13 -0400

    changer nom du dossier

--- src/components/button/__test__/button.test.js
+++ src/components/button/__test__/button.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import Button from './../button';
+import Button from '../button';
 
 import { render, cleanup } from '@testing-library/react';
 import "@testing-library/jest-dom/extend-expect";
