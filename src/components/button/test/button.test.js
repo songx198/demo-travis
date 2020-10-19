3fb17228e6c4de15f281a47516396c24cb4e309e
songx198 <moko1125986@gmail.com>
2020-10-18 23:03:42 -0400

    ajouter des tests

---
+++ src/components/button/test/button.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import Button from './../button';
+
+import { render, cleanup } from '@testing-library/react';
+import "@testing-library/jest-dom/extend-expect";
+
+import render2 from "react-test-renderer";
+
+//effacer les donnes apres chaque test
+afterEach(cleanup);
+
+it("test button", () => {
+    const div = document.createElement("div");
+    ReactDom.render(<Button></Button>, div)
+})
+
+//tester si le Text Content de mon button est-t-il égale a "Click"
+it("test label du button", () => {
+    const { getByTestId } = render(<Button label="Click"></Button>)
+    expect(getByTestId('button')).toHaveTextContent("Click")
+})
+
+//pour regarder les differences entre les anciens test et les new tests
+it("test snapshot", () => {
+    const tree = render2.create(<Button label="save"></Button>).toJSON();
+    expect(tree).toMatchSnapshot();
+})
+
+
